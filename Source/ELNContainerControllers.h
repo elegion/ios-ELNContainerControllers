7a17f7214485a1759cd70fbc67d580f0dfbabcfe
chebur <chebur.mail@gmail.com>
2016-04-15 16:02:14 +0300

    Configure umbrella header

--- Source/ELNContainerControllers.h
+++ Source/ELNContainerControllers.h
@@ -14,6 +14,7 @@ FOUNDATION_EXPORT double ELNContainerControllersVersionNumber;
 //! Project version string for ELNContainerControllers.
 FOUNDATION_EXPORT const unsigned char ELNContainerControllersVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ELNContainerControllers/PublicHeader.h>
+#import <ELNContainerControllers/ELNContainerViewController.h>
+#import <ELNContainerControllers/ELNScrollViewController.h>
 
 
